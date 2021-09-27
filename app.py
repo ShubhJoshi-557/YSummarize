a7e978642a3aba4acd286b3f27b75c6d36e06be6
Shubh Joshi <shubhjoshi80@gmail.com>
2021-09-27 11:20:59 +0530

    handled errors

--- app.py
+++ app.py
@@ -23,41 +23,52 @@ def summarize():
     youtube_url = request.args.get('youtube_url')
     video_id = youtube_url.split("=")[1]
     # video_id = 'aOL7wzEIZSc'
-    transcript_json = YouTubeTranscriptApi.get_transcript(video_id , languages=['en'])
-    transcript_text = ""
-    count = 0
-    for i in transcript_json:
-        transcript_text += " " + i['text']
+    try:
+        transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
+        transcript =""
+        transcript = transcript_list.find_transcript(['en','en_IN'])
+    except:
+        summary1 = "This video either has Transcripts Disabled or They are not available in english!"
+        return summary1
+    if(transcript!=""):
+        transcript_json = YouTubeTranscriptApi.get_transcript(video_id, languages=['en','en-IN'])
+        transcript_text = ""
+        count = 0
+        for i in transcript_json:
+            transcript_text += " " + i['text']
 
-    # summarizer = pipeline('summarization')
-    # num_iters = int(len(transcript_text)/1000)
-    # summarized_text = []
-    # for i in range(0, num_iters + 1):
-    #     start = 0
-    #     start = i * 1000
-    #     end = (i + 1) * 1000
-    #     out = summarizer(transcript_text[start:end])
-    #     out = out[0]
-    #     out = out['summary_text']
-    #     summarized_text.append(out)
-    #     summary = ""
-    #     for i in summarized_text:
-    #         summary += " " + i
-#======================================================================================================================
-    model = T5ForConditionalGeneration.from_pretrained("t5-base")
-    tokenizer = T5Tokenizer.from_pretrained("t5-base")
-    inputs = tokenizer.encode("summarize: " + transcript_text, return_tensors="pt", max_length=512, truncation=True)
-    outputs = model.generate(
-        inputs, 
-        max_length=150, 
-        min_length=40, 
-        length_penalty=2.0, 
-        num_beams=4, 
-        early_stopping=True)
-    summary = tokenizer.decode(outputs[0])
-    summary1 = summary.removeprefix("<pad>").removesuffix("</s>")
-#======================================================================================================================
-    return summary1
+        # summarizer = pipeline('summarization')
+        # num_iters = int(len(transcript_text)/1000)
+        # summarized_text = []
+        # for i in range(0, num_iters + 1):
+        #     start = 0
+        #     start = i * 1000
+        #     end = (i + 1) * 1000
+        #     out = summarizer(transcript_text[start:end])
+        #     out = out[0]
+        #     out = out['summary_text']
+        #     summarized_text.append(out)
+        #     summary = ""
+        #     for i in summarized_text:
+        #         summary += " " + i
+    #======================================================================================================================
+        model = T5ForConditionalGeneration.from_pretrained("t5-base")
+        tokenizer = T5Tokenizer.from_pretrained("t5-base")
+        inputs = tokenizer.encode("summarize: " + transcript_text, return_tensors="pt", max_length=512, truncation=True)
+        outputs = model.generate(
+            inputs, 
+            max_length=150, 
+            min_length=40, 
+            length_penalty=2.0, 
+            num_beams=4, 
+            early_stopping=True)
+        summary = tokenizer.decode(outputs[0])
+        summary1 = summary.removeprefix("<pad>").removesuffix("</s>")
+    #======================================================================================================================
+        return summary1
+    else:
+        summary1 = "This video either has Transcripts Disabled or They are not available in english!"
+        return summary1
 
 # server the app when this file is run
 if __name__ == '__main__':
